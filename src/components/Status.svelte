6eaea07574f1c2b509d65aace4b0d8a7621eb122
Ismail Sacic <cicas03@hotmail.com>
2022-11-07 21:02:35 +0100

    Added content and styling to home page

---
+++ src/components/Status.svelte
@@ -0,0 +1,100 @@
+<script>
+    import ProgressBar from 'progressbar.js';
+    import { onMount } from 'svelte';
+
+    let goal = 250_000;
+    let raised = 51_312;
+    let raised_percent = (raised / goal);
+    
+    let progressbar = null;
+    let progressbar_text_span = document.createElement('span');
+    progressbar_text_span.innerText = ` kr `;
+    progressbar_text_span.className = 'goal-text';
+
+    onMount(async () => {
+
+        progressbar = new ProgressBar.Line('#progress', {
+            color: '#eee',
+            trailColor: '#eee',
+            trailWidth: 1,
+            duration: 1400,
+            easing: 'bounce',
+            strokeWidth: 4,
+            from: { color: '#b0ff45', a: 0 },
+            to: { color: '#64813d', a: 1 },
+            step: (state, circle) => {
+                circle.path.setAttribute('stroke', state.color);
+            }
+        });
+        progressbar.animate(raised_percent);
+        progressbar.setText(` 💵 ${raised.toLocaleString('fr-FR')} `);
+        progressbar.text.appendChild(progressbar_text_span);
+    });
+
+
+    $: progress = raised / goal * 100;
+
+</script>
+<div class="whole_progress">
+    <div class="whole_text">
+        Koliko smo dosada skupili para 💰
+    </div>
+    <p>
+        <span>0 kr</span>
+        <span>Cilj {goal.toLocaleString('fr-FR')} kr</span>
+    </p>
+    <div id="progress"></div>
+</div>
+
+
+<style lang="scss">
+
+    .whole_progress {
+        width: 100%;
+        height: 20vh;
+
+        p {
+            display: flex;
+            justify-content: space-between;
+            margin: 0;
+            padding: 10px;
+            font-size: 1rem;
+            align-items: center;
+        }
+        .whole_text{
+            background-color: #3c3c3c;
+            padding: 0.4rem;
+            border-radius: 0.2rem;
+        }
+    }
+
+    :global(.progressbar-text) {
+        font-size: 1.5rem;
+        margin-top: 3rem !important;
+        font-weight: 800;
+    }
+    :global(.goal-text) {
+        font-size: 1rem;
+        font-weight: 400;
+    }
+
+    @media only screen and (max-width: 600px) {
+        :global(.progressbar-text) {
+            position: unset !important;
+            top: unset !important;
+            left: unset !important;
+            transform: unset !important;
+
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: row;
+            margin-top: 1rem !important;
+
+            :global(span){
+                margin: 0 0.5rem;
+                margin-top: 0.4rem ;
+            }
+        }
+    }
+</style>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,7 +1,20 @@
+<script>
+    import Status from "../components/Status.svelte";
+</script>
 <div class="container">
     <div class="backbround"></div>
     <div class="content">
-        <h1>Projekt Bosna</h1>
+        <h1>Projekat Bosna 🇧🇦</h1>
+        <p class="info">
+            Mi, Bemuf Göteborg, zajedno sa Islamic Reliefom i Islamskom zajednicom Bosnjaka Göteborg pokrenuli smo projekat u Bosni i Hercegovini čiji je cilj renoviranje sirotišta ”JU DOM-porodica” u Zenici. 
+        </p>
+        <Status />
+        <a href="/info">Više informacije </a>
+        <div class="sponsor">
+            <img src="/imgs/bemuf_lgo.png" alt="Sponsor">
+            <img src="/imgs/islamic_relief.jpg" alt="Sponsor">
+            <img src="/imgs/izgbg.jpeg" alt="Sponsor">
+        </div>
     </div>
   </div>
 
@@ -15,7 +28,7 @@
         
         display: grid;
         grid-template-columns: 1fr 6fr 1fr;
-        grid-template-rows: 1fr 6fr 1fr;
+        grid-template-rows: 0.4fr auto 1fr;
         grid-template-areas: 
         '. . .'
         '. content .'
@@ -26,7 +39,7 @@
 
     .backbround {
         width: 100vw;
-        height: 100vh;
+        height: 110vh;
         
         grid-area: 1/1/5/5;
         background: linear-gradient(0deg, rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.70)), url("/imgs/bosna-background.webp");
@@ -37,18 +50,72 @@
     }
 
     .content {
-        height: 200px;
-        position: relative;
+        height: 100%;
         
         grid-area: content;
         display: flex;
+        flex-direction: column;
+        gap: 1rem;
         
-        justify-content: center;
-        align-items: center;
         color: white;
+        background-color: #2a2a2a;
+        padding: 50px 30px;
+        border-radius: 16px;
+        
         h1{
             font-size: 4rem;
         }
+        p{
+            font-size: 1.3rem;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            font-weight: 500;
+        }
+        a{
+            font-size: 1.3rem;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            font-weight: 700;
+            background-color: #3b88c3;
+            border-radius: 0.3rem;
+            text-decoration: none;
+            text-align: center;
+            padding:  0.5rem 1rem;
+            color: white;
+            &:hover{
+                opacity: 0.8;
+            }
+
+        }
+     
+        .sponsor{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 2rem;
+
+            img{
+                width: 100px;
+                height: 100px;
+                border-radius: 50%;
+            }
+        }
+
+    }
+    @media only screen and (max-width: 600px) {
+        .content {
+            padding: 40px 30px;
+            height: auto;
+            h1{
+                font-size: 14vw;
+            }
+            p{
+                font-size: 4vw;
+            }
+            .sponsor img {
+                width: 15vw;
+                height: 15vw;
+            }
+        }
+
     }
 
             
